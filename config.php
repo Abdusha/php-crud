a84bb83f59116fd88dab6985a729e2aab717243d
Abdurrahman Shaleh <shaleh2105@gmail.com>
2018-04-27 23:47:54 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,17 @@
 <?php 
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$db = "db_gd_fri";
+	//localhost dengan database db_gd_fri.sql
+	//$host = "localhost"; 
+	//$user = "root";
+	//$pass = "";
+	//$db = "db_gd_fri";
+
+	//for openshift
+	$host = getenv("MYSQL_SERVICE_HOST");
+	$port = getenv("MYSQL_SERVICE_PORT");
+	$server = getenv("databaseserver");
+	$user = getenv("databaseuser");
+	$pass = getenv("databasepass");
+	$db = getenv("databasename"); 
 
 	$conn = mysqli_connect($host, $user, $pass, $db);
 	
